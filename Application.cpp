49379cb28ed5219c99216491555678a359664dfd
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2023-04-04 23:10:24 +0200

    Add aplication rendering part

---
+++ Application.cpp
@@ -0,0 +1,19 @@
+#include "Application.h"
+
+#include "imgui.h"
+
+namespace AgentsApp
+{
+
+	void RenderUI()
+	{
+		ImGui::Begin("Settings");
+		ImGui::Button("Hello");
+
+		static float value = 0.0f;
+
+		ImGui::DragFloat("Value", &value);
+		ImGui::End();
+	}
+
+}
---
+++ Application.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace AgentsApp
+{
+	void RenderUI();
+}
