404cc3ef114ae7c2f5b23ccfac07e3f300385e83
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2023-04-18 13:18:09 +0200

    Refactoring v2

--- Application.h
+++ Application.h
@@ -1,7 +1,22 @@
 #pragma once
+#include <cstddef>
+#include "imgui.h"
 
 namespace AgentsApp
 {
+	class AgentsAppLog {
+	private:
+		ImGuiTextBuffer     Buf;
+		ImGuiTextFilter     Filter;
+		ImVector<int>       LineOffsets; // Index to lines offset. We maintain this with AddLog() calls.
+		bool                AutoScroll;  // Keep scrolling if already at the bottom.
+	public:
+		void Clear();
+		void AddLog(const char* fmt, ...);
+		void Draw(const char* title, bool* p_open = NULL);
+		AgentsAppLog();
+	};
+
 	void RenderUI();
 	void ShowRbtsmWindow();
 }
